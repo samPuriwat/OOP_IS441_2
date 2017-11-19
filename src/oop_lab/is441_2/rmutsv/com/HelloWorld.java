d3ad557ba22aa425c9cfae0e53874e2afa11310f
samPuriwat <sam.puriwat@gmail.com>
2017-11-19 15:21:20 +0700

    Edit information

--- src/oop_lab/is441_2/rmutsv/com/HelloWorld.java
+++ src/oop_lab/is441_2/rmutsv/com/HelloWorld.java
@@ -7,8 +7,8 @@ public static void main(String[] args) {
 
     System.out.println("Hello World, I'am SAM");
     System.out.println("Puriwat Lertkrai");
-    System.out.println("Information System");
-    System.out.println("MT: RMUTSV");
+    System.out.println("Information System, MT:RMUTSV");
+
 
 
 }// main
