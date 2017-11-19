c45de755b50493fc4ee56a832f5074fa57e9582d
samPuriwat <sam.puriwat@gmail.com>
2017-11-19 14:51:00 +0700

    Testing upload to github

---
+++ src/oop_lab/is441_2/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lab.is441_2.rmutsv.com;
+
+public class HelloWorld {
+
+//    create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello World, I'am SAM");
+
+
+}// main
+
+}// class
